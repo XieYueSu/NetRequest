12581f39b5609101aa334f0dc3dfb04bfd3e6854
xieys <xieys@gzsendi.cn>
2020-09-11 15:22:59 +0800

    1235555

---
+++ app/src/main/java/com/sendi/netrequest/LogController2.java
@@ -0,0 +1,14 @@
+package com.sendi.netrequest;
+
+/**
+ * Created by xieys on 2020/8/1.
+ */
+public class LogController2 {
+    public void upload(){
+
+        //1234567999
+
+
+
+    }
+}
---
+++ app/src/main/java/com/sendi/netrequest/LogController3.java
@@ -0,0 +1,13 @@
+package com.sendi.netrequest;
+
+/**
+ * Created by xieys on 2020/8/1.
+ */
+public class LogController3 {
+    public void upload(){
+
+        //122222222
+
+
+    }
+}
